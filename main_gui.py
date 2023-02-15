142b072be315e6ba6ab23263525093c6b053ca93
Valery Klimenko <valery.klimenko@ntprog.com>
2023-02-15 20:13:40 +0300

    Improve UI and add +1m button
    1) Set the window to the middle of main monitor
    2) Added the ability to add 1 minute to WL in GUI mode

--- main_gui.py
+++ main_gui.py
@@ -1,10 +1,31 @@
 from common import format_time, get_link_by_description, get_key_by_description, get_description_without_jira_key, \
     get_sorted_aggregated_time_points, convert_timestamp_from_toggle_to_jira
 from jira_wl_updater import jira_add_wl
+from screeninfo import get_monitors
 
 import PySimpleGUI as sg
 
 font = ("Helvetica", 20)
+
+
+def get_default_screen_dimensions():
+    default_monitor = [monitor for monitor in get_monitors() if monitor.is_primary][0]
+    screen_width, screen_height = default_monitor.width, default_monitor.height
+    return screen_width, screen_height
+
+
+class Window(sg.Window):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs, finalize=True)
+        self.move_to_center()
+
+    def move_to_center(self):
+        if not self._is_window_created('tried Window.move_to_center'):
+            return
+        screen_width, screen_height = get_default_screen_dimensions()
+        win_width, win_height = self.size
+        x, y = (screen_width - win_width) // 2, (screen_height - win_height) // 2
+        self.move(x, y)
 
 
 def get_current_layout(current_wl, entered_key=None):
@@ -23,13 +44,16 @@ def get_current_layout(current_wl, entered_key=None):
 
     if key:
         result += [[sg.Text(f'Key: {key}', font=font)],
-                [sg.Text('Enter WL:', font=font), sg.InputText(
-                    default_text=get_description_without_jira_key(current_wl.description), font=font)],
-                [sg.Button('Re-enter key', font=font), sg.Button('Log', bind_return_key=True, font=font)]]
+                   [sg.Text('Enter WL:', font=font),
+                    sg.InputText(
+                        default_text=get_description_without_jira_key(current_wl.description), font=font)],
+                   [sg.Button('Re-enter key', font=font),
+                    sg.Button('Log', bind_return_key=True, font=font),
+                    sg.Button('+1m', font=font)]]
     else:
         result += [[sg.Text(f'Cannot parse key :(', font=font)],
-                [sg.Text('Enter Key:', font=font), sg.InputText(font=font)],
-                [sg.Button('Save key', font=font, bind_return_key=True)]]
+                   [sg.Text('Enter Key:', font=font), sg.InputText(font=font)],
+                   [sg.Button('Save key', font=font, bind_return_key=True)]]
 
     return result
 
@@ -43,13 +67,16 @@ def run():
     current_layout = get_current_layout(current_wl)
 
     while True:
-        window = sg.Window('Toggle Jira Helper', current_layout)
+        window = Window('Toggle Jira Helper', current_layout)
         event, values = window.read()
         if event == sg.WIN_CLOSED or event == 'Cancel':
             # if user closes window or clicks cancel
             break
         if event == 'Save key':
             entered_key = values[0]
+            current_layout = get_current_layout(current_wl, entered_key)
+        elif event == '+1m':
+            current_wl.duration += 60
             current_layout = get_current_layout(current_wl, entered_key)
         elif event == 'Re-enter key':
             current_layout = get_current_layout(current_wl, "")
